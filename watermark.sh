207007542600f14b0d206d6d4f67118fbc70499f
moomoon <gmoomoon@gmail.com>
2016-02-18 10:56:43 +0800

    break when receive signal 2

--- watermark.sh
+++ watermark.sh
@@ -10,14 +10,14 @@ mkdir -p $tempPath;
 rm -rf $outputPath;
 mkdir -p $outputPath;
 
-trap "exit" INT
-
 for file in $sourcePath/*; do
     fileName=$(basename ${file});
     tempFile="${tempPath}/${fileName}";
     outputFile="${outputPath}/${fileName}";
     echo $fileName;
     ffmpeg -i $file  -vf "scale=(iw * 640 / min(iw\, ih)):(ih * 640 / min(iw\, ih)),crop=640:640,setdar=1/1,setsar=1/1" $tempFile;
+    test $? -gt 128 && break;
     ffmpeg -i $tempFile -i $watermark -filter_complex "overlay=480:20" -codec:a copy $outputFile;
+    test $? -gt 128 && break;
 done
 
